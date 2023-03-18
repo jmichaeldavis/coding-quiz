f3f2e29b2700cff4692eba473bd1300ed63797d9
jmichaeldavis <jmichaeldavis17@gmail.com>
2023-03-18 16:21:05 -0600

    added html structure

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Variables Section
+
+
+/* Game Function - Includes if else statement
+ (if user selects the answer that is equal to true, then move to next question, else remove time from clock)
+ If they get it wrong, the answer gets grayed out. It only moves to next question when answered correctly */
+
+ /* Timer Function - Some way to visually let them know they lost time when answered incorrectly */
+
+ // Local Storage - High score function
+
+ // Start button to initiate quiz
